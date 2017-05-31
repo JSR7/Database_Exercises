6626e7cb0d7c72efe33cbb4c83517fe8a384a95e
JSR7 <G!thUb1!>
2017-05-31 13:50:53 -0500

    update album listing

---
+++ albums.seeder.sql
@@ -0,0 +1,7 @@
+use codeup_test_db;
+insert into albums(artist, name, release, sales, genre)
+value ('Shania Twain', 'Come on Over', 1997, 29.6, 'Country'),
+  ('Led Zeppelin',	'Led Zeppelin IV',	1971	,  29.0,'rock' ),
+  ('michael jackson', 'bad', 1971, 29.0, 'folk' ),
+  ('Alanis Morissette', 'Jagged Little Pill', 1995, 24.4, 'Alternative Rock'),
+  ('Celion Dion', 'Falling into You', 1996, 20.2, 'Pop'),
