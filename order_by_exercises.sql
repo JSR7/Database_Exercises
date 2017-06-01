c5ae0498e4b4be93663293fde3c84b52b76134c9
JSR7 <G!thUb1!>
2017-06-01 16:52:52 -0500

    order by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -18,5 +18,13 @@ select * from employees where last_name like '%E%' order by employee_number;
 select * from employees where last_name like '%E%' order by employee_number DESC;
 
 
-select * from employees where employment_date BETWEEN 1990 01 01 and 1999 12 31 order by 'age' and 'employment_date' DESC ;
+select * from employees where employment_date BETWEEN 1990 01 01 AND 1999 12 31 order by 'age' and 'employment_date' DESC ;
 
+
+select *
+from employees
+where birth_date 
+like '%-12-25'
+AND hire_date
+like '199%'
+order by birth_date, hire_date DESC;
