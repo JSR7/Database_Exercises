b61951ef6843a3ea4c59e789db2cf3e386a5acb8
JSR7 <G!thUb1!>
2017-06-01 10:19:08 -0500

    added delete exercises file

--- DeleteExercises.sql
+++ DeleteExercises.sql
@@ -1,3 +1,6 @@
+use codeup_test_db;
+
+
 select * from albums where release_date > 1991;
 
 
@@ -8,10 +11,11 @@ select * from albums where artist = 'Whitney Houston';
 
 
 
+select 'deleting disco albums';
 
 
 
+delete from albums where genre = 'disco';
+delete from albums where release_date > 1991;
+delete from albums where artist = 'Whitney Houston';
 
-delete * from albums where genre = 'disco';
-delete * from albums where release_date > 1991;
-delete * from albums where artist = 'Whitney Houston';
