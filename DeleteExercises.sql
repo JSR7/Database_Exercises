ced6b8b54adc8ecd63e99bb3ee67818aead6c965
JSR7 <G!thUb1!>
2017-06-01 09:56:36 -0500

    added delete exercises file

---
+++ DeleteExercises.sql
@@ -0,0 +1,17 @@
+select * from albums where release_date > 1991;
+
+
+select * from albums where genre = 'disco';
+
+
+select * from albums where artist = 'Whitney Houston';
+
+
+
+
+
+
+
+delete * from albums where genre = 'disco';
+delete * from albums where release_date > 1991;
+delete * from albums where artist = 'Whitney Houston';
